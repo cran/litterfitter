9700858c6983d6dedb96892d0ffd340414ea66ca
Will Cornwell <wcornwell@gmail.com>
2022-09-15 05:30:02 +0000

    version 0.1.2

---
+++ R/pineneedles-data.R
@@ -0,0 +1,10 @@
+##' @name pineneedles
+##' @title decomposition trajectory for pine needles
+##' @description data from Hobbie et al means of pine needle decomposition
+##' @docType data
+##' @usage data(pineneedles)
+##' @format a data.frame with two columns
+##' @source Hobbie et al.
+##' @author Will Cornwell 
+"pineneedles"
+
---
+++ inst/doc/litterfitter.R
@@ -0,0 +1,33 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(litterfitter)
+
+## ----results="hide",warning=FALSE,message = FALSE-----------------------------
+fit <- fit_litter(time=c(0,1,2,3,4,5,6),
+                  mass.remaining =c(1,0.9,1.01,0.4,0.6,0.2,0.01),
+                  model="weibull",
+                  iters=500)
+
+class(fit)
+
+## ----fig.height=6,results='hide',fig.keep=TRUE,warning=FALSE,message = FALSE----
+plot_multiple_fits(time=c(0,1,2,3,4,5,6),
+                   mass.remaining=c(1,0.9,1.01,0.4,0.6,0.2,0.01),
+                   model=c("neg.exp","weibull"),
+                   iters=500)
+
+## ----fig.keep=TRUE------------------------------------------------------------
+   plot(fit)
+
+## ----echo=FALSE,fig.keep=TRUE-------------------------------------------------
+   summary(fit)
+
+## ----echo=FALSE,fig.keep=TRUE-------------------------------------------------
+   post<-bootstrap_parameters(fit)
+   plot(post)
+
